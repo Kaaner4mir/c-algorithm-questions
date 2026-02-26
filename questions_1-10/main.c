dc247aa1cd711edda93c6c0fd74d931722175f63
Emirhan Kaaner <kaaner4mir@gmail.com>
2026-02-26 04:09:17 +0300

    initial commit

---
+++ questions_1-10/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // 1. Soru: 2 sayının toplamını veren programı yazınız.
+
+    // int num1, num2;
+    // int sum = 0;
+
+    // printf("Birinci sayiyi girin: ");
+    // scanf("%d", &num1);
+
+    // printf("Ikinci sayiyi girin: ");
+    // scanf("%d", &num2);
+
+    // sum = num1 + num2;
+
+    // printf("%d + %d = %d", num1, num2, sum);
+}
